c907ff84779d53db477b204fcf7630a34e99d2ed
Nathan Patterson <imnathanpatterson@gmail.com>
2024-08-29 12:38:02 -0400

    Began setup for polybase test environment, SQL (psql) db is initialized and configured (config folder), webpack is configured and  serving, npm start will run react app on port 9000 which querys psql for users and displays them on screen. Next up is mongodb, redis, and neo4j

---
+++ Test-Env/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
