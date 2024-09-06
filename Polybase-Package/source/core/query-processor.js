671bda2e7fef27f6e158f4de3e2da83f7852e290
Alazar Aklilu <zar.aklilu@gmail.com>
2024-09-05 22:27:57 -0400

    notes

--- Polybase-Package/source/core/query-processor.js
+++ Polybase-Package/source/core/query-processor.js
@@ -4,11 +4,13 @@
  * handles the processing and validation of database queries.
  * Breaks down queries into components, validates them, and preps them for 
  * execution in the corresponding adapter in the transformation layer.
+ * 
+ * Layman's: It takes apart and checks database questions (queries) to make sure they’re correct, then gets them ready to be run by the system that handles the actual data.
  */
 
 
 /**
- * Receives a parsed query from presentation layer an inits an execution
+ * Receives a parsed query from presentation layer and inits an execution
  * plan for the query based on requirements 
  * @param {String} query Operation requesed by user
  * @returns {Object} Clearly-partitioned execution plan 
--- Polybase-Package/source/core/sychronization-engine.js
+++ Polybase-Package/source/core/sychronization-engine.js
@@ -4,6 +4,8 @@
  * manages synchronization of data across diff databases. 
  * Focus on data consistency & resolving conflicts --generates synchronization plans and 
  * applies changes to the target dbs based on these plans.
+ * 
+ * Layman's: It keeps data in sync across different databases, making sure everything matches up and fixing any differences, by creating and following a plan to update the databases.
  */
 
 function synchronizeData(source, target) {
