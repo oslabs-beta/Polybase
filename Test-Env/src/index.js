79d6c2424ebfceb5034744908e201ded6d6ab5e6
Nathan Patterson <imnathanpatterson@gmail.com>
2024-09-12 20:27:14 -0400

    mdb database is now being shown on screen. Data retretrieval is being handled serverside. Next steps: present redis and neo data on screen

--- Test-Env/src/index.js
+++ Test-Env/src/index.js
@@ -5,11 +5,13 @@ const connectDB = require('../config/mongoConfig.js');
 const mongoUser = require('../models/mongoUser.js');
 
 function App() {
-  const [users, setUsers] = useState([]);
+  const [psqlUsers, setPsqlUsers] = useState([]);
+  const [mongoUsers, setMongoUsers] = useState([]);
+  const [redisLeaderboard, setRedisLeaderboard] = useState([]);
 
   // Fetch users from Supabase when the component mounts
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchPsqlUsers = async () => {
       console.log('entered psql data fetch')
       console.log(supabase);
 
@@ -19,22 +21,80 @@ function App() {
       if (error) {
         console.error('Error fetching users:', error);
       } else {
-        setUsers(data);
+        setPsqlUsers(data);
       }
     };
 
-    fetchUsers();
+    fetchPsqlUsers();
     console.log('invoked psql data fetch')
   }, []);
+
+  // fetch users from mdb via backend
+  useEffect(() => {
+    const fetchMongoUsers = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/mongo-users');
+        const data = await response.json();
+        setMongoUsers(data);
+      } catch(error) {
+        console.error('Error fetching mdb users from api:', error);
+      }
+    };
+    fetchMongoUsers();
+  }, []);
+
+  useEffect(() => {
+    const fetchRedisLeaderboard = async () => {
+      try {
+        const response = await fetch('/redis-leaderboard');
+        const leaderboard = await response.json();
+        setRedisLeaderboard(leaderboard);
+      } catch(err) {
+        console.error('Error fetching redis data:', err);
+      }
+    };
+    fetchRedisLeaderboard();
+  }, []);
+
   return (
     <div>
       <h1>Polybase Test Environment</h1>
       <h2>PSQL Users</h2>
       <ul>
-        {users.map(user => (
+        {psqlUsers.map(user => (
           <li key={user.id}>{user.username}  |  id:{user.id}</li>
         ))}
       </ul>
+
+      <h2>MDB Users</h2>
+      <ul>
+      {mongoUsers.map((user) => (
+          <li key={user._id}>
+            <strong>Username:</strong> {user.username}
+            <ul>
+              <li>
+                <strong>Followers:</strong> 
+                {user.followers && user.followers.length > 0
+                  ? user.followers.join(', ')
+                  : 'No followers'}
+              </li>
+              <li>
+                <strong>Following:</strong> 
+                {user.following && user.following.length > 0
+                  ? user.following.join(', ')
+                  : 'Not following anyone'}
+              </li>
+            </ul>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Redis Leaderboard</h2>
+      <ol>
+        {redisLeaderboard.map((entry, index) => {
+          <li key={index}>{entry.value} - {entry.score} followers</li>
+        })}
+      </ol>
     </div>
   );
 }
