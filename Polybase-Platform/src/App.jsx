e92fdd42e378844b235eb48346fcd1ab830ead96
Nathan Patterson <imnathanpatterson@gmail.com>
2024-10-19 13:17:38 -0400

    imported structural boiler plate for polybase platform

---
+++ Polybase-Platform/src/App.jsx
@@ -0,0 +1,19 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
+      
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
---
+++ Polybase-Platform/src/components/Hero.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Section from "./Section";
+
+const Hero = () => {
+  return <Section className="pt-[12rem] -mt-[5.25]">Hero</Section>;
+};
+
+export default Hero;
