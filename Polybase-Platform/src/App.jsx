f54cabe0e545d95bd8e1f1a9d72c1ee4462914fc
Nathan Patterson <imnathanpatterson@gmail.com>
2024-10-23 13:01:48 -0400

    implemented collaboration and services section

--- Polybase-Platform/src/App.jsx
+++ Polybase-Platform/src/App.jsx
@@ -1,7 +1,9 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -9,6 +11,9 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
       </div>
       
       <ButtonGradient />
--- Polybase-Platform/src/components/Hero.jsx
+++ Polybase-Platform/src/components/Hero.jsx
@@ -7,6 +7,7 @@ import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 import Generating from "./Generating";
 import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -107,6 +108,7 @@ const Hero = () => {
 
           <BackgroundCircles />
         </div>
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block"/>
       </div>
       <BottomLine />
     </Section>
