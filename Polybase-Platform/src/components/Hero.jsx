9070ff59d8e6921296f58dd573d2128d9337415b
Nathan Patterson <imnathanpatterson@gmail.com>
2024-11-26 10:20:39 -0500

    cleaned up unnecessary imports and commented out code

--- Polybase-Platform/src/components/Hero.jsx
+++ Polybase-Platform/src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { curve, robot, database, heroBackground } from "../assets";
+import { curve, database } from "../assets";
 import Button from "./Button";
 import Section from "./Section";
 import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
@@ -7,7 +7,6 @@ import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 import Generating from "./Generating";
 import Notification from "./Notification";
-// import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -27,8 +26,7 @@ const Hero = () => {
         [6rem]"
         >
           <h1 className="h1 mb-6">
-            Multi-Database Enviroments Made Seamless with{" "}
-            {` `}
+            Multi-Database Enviroments Made Seamless with {` `}
             <span className="inline-block relative">
               Polybase{" "}
               <img
@@ -44,9 +42,12 @@ const Hero = () => {
             className="body-1 max-w-3xl mx-auto mb-6
           text-n-2 lg:mb-8"
           >
-            A JavaScript library designed to simplify data integration and synchronization within polyglot environments.
+            A JavaScript library designed to simplify data integration and
+            synchronization within polyglot environments.
           </p>
-          <Button white><a href="https://www.npmjs.com/package/polybase-package">Install</a></Button>
+          <Button white>
+            <a href="https://www.npmjs.com/package/polybase-package">Install</a>
+          </Button>
         </div>
         <div
           className="relative max-w-[23rem] mx-auto
@@ -67,8 +68,10 @@ const Hero = () => {
                   alt="database image"
                 />
 
-                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem]
-                 md:-translate-x-1/2" />
+                <Generating
+                  className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem]
+                 md:-translate-x-1/2"
+                />
 
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul
@@ -95,19 +98,9 @@ const Hero = () => {
 
             <Gradient />
           </div>
-          {/* <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
-            <img
-              src={heroBackground}
-              className="w-full"
-              width={1440}
-              height={1800}
-              alt="hero"
-            />
-          </div> */}
 
           <BackgroundCircles />
         </div>
-        {/* <CompanyLogos className="hidden relative z-10 mt-20 lg:block"/> */}
       </div>
       <BottomLine />
     </Section>
